5bdc059e836d3e4a04c9f19435dfe0997e38e347
kwwalton <kennethwwalton@gmail.com>
2024-06-05 09:23:21 -0400

    minor styling on coupon and coupon input

--- src/components/coupons/coupon-input.tsx
+++ src/components/coupons/coupon-input.tsx
@@ -17,26 +17,30 @@ export default function CouponInput({ cartId }: Readonly<ICouponInputProps>) {
     addCouponToCart(couponToAdd, cartId).then(() => setIsVisible(!isVisible))
   }
 
-  // move the form and the is visible state to its own component
+  // TODO: move the form and the is visible state to its own component
   return (
     <div>
       {isVisible && (
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="coupon">
-            enter coupon
-            <input type="text" name="coupon" />
+        <form onSubmit={handleSubmit} className="flex">
+          <label htmlFor="coupon" className="flex flex-col text-xs">
+            coupon
+            <input type="text" name="coupon" className="text-md p-3" />
           </label>
-          <button type="submit">submit</button>
+          <button
+            type="submit"
+            className="px-4 py-2 text-sm rounded-md shadow-sm opacity-100 mt-4"
+          >
+            Apply
+          </button>
         </form>
       )}
 
-      {isVisible ? (
-        <button onClick={() => setIsVisible(!isVisible)}>Close</button>
-      ) : (
-        <button onClick={() => setIsVisible(!isVisible)}>
-          Add a promo code
-        </button>
-      )}
+      <button
+        onClick={() => setIsVisible(!isVisible)}
+        className="underline text-sm mb-3"
+      >
+        {isVisible ? 'Cancel' : 'Add a promo code'}
+      </button>
     </div>
   )
 }
--- src/components/coupons/coupon.tsx
+++ src/components/coupons/coupon.tsx
@@ -11,10 +11,13 @@ export default function Coupon({ code, cartId }: Readonly<ICouponProps>) {
   const { pending } = useFormStatus()
 
   return (
-    <li style={{ color: pending ? 'lightgrey' : 'black' }}>
-      {code} &nbsp;
+    <li
+      style={{ color: pending ? 'lightgrey' : 'black' }}
+      className="flex items-center mb-3"
+    >
+      <span className="text-lime-600">{code} applied &nbsp;</span>
       <form action={async () => await removeCouponFromCart(code, cartId)}>
-        <button type="submit" disabled={pending}>
+        <button type="submit" disabled={pending} className="underline text-sm">
           remove
         </button>
       </form>
